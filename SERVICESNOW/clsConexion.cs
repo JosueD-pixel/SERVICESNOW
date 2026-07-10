099dfc07ef1f695f4113340f3a99c10fb8f44059
Angel Josue Domingo Guillen <angel_domingoguillen07@hotmail.com>
2026-07-10 00:44:35 -0600

    Conexion  y clase login por angel

---
+++ SERVICESNOW/clsConexion.cs
@@ -0,0 +1,36 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SERVICESNOW
+{
+    internal class clsConexion
+    {
+        private string host = "localhost";
+        private string bd = "servicesnow";
+        private string usuario = "root";
+        private string password = "";
+        private string puerto = "3306";
+
+        private string cadenaConexion => $"server= {host};database={bd};user={usuario};password={password};port={puerto}";
+
+        //Método Abrir conexcion
+        public MySqlConnection AbrirConexion()
+        {
+            var conexion = new MySqlConnection(cadenaConexion);
+            try
+            {
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar conectarse a la base de datos" + ex.Message, ex);
+            }
+
+        }
+
+    }
+}
