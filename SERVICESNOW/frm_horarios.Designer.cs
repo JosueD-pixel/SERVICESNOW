b4ab8f43ad1a3130029aacc2ee3d3bd98bb8427f
luis angel mercado hernandez <mercadohernandezluisangel66@gmail.com>
2026-07-14 15:04:56 -0600

    elaboracion de frm horarios por luis angel

---
+++ SERVICESNOW/frm_horarios.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace SERVICESNOW
+{
+    partial class frm_horarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnl_superior = new Panel();
+            pn_formulario = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            this.btn_eliminar = new PictureBox();
+            this.btn_agregar = new PictureBox();
+            btn_modificar = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox4 = new PictureBox();
+            panel1 = new Panel();
+            label8 = new Label();
+            dgv_horarios = new DataGridView();
+            txt_buscar = new TextBox();
+            cmb_horaInicio = new ComboBox();
+            cmb_horafinal = new ComboBox();
+            pnl_superior.SuspendLayout();
+            pn_formulario.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)this.btn_eliminar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)this.btn_agregar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btn_modificar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv_horarios).BeginInit();
+            SuspendLayout();
+            // 
+            // pnl_superior
+            // 
+            pnl_superior.BackColor = Color.FromArgb(92, 64, 51);
+            pnl_superior.Controls.Add(pictureBox4);
+            pnl_superior.Controls.Add(label5);
+            pnl_superior.Controls.Add(label4);
+            pnl_superior.Dock = DockStyle.Top;
+            pnl_superior.Location = new Point(0, 0);
+            pnl_superior.Name = "pnl_superior";
+            pnl_superior.Size = new Size(1630, 76);
+            pnl_superior.TabIndex = 0;
+            // 
+            // pn_formulario
+            // 
+            pn_formulario.BackColor = Color.FromArgb(246, 240, 232);
+            pn_formulario.Controls.Add(cmb_horafinal);
+            pn_formulario.Controls.Add(cmb_horaInicio);
+            pn_formulario.Controls.Add(label3);
+            pn_formulario.Controls.Add(label2);
+            pn_formulario.Controls.Add(label1);
+            pn_formulario.Location = new Point(110, 128);
+            pn_formulario.Name = "pn_formulario";
+            pn_formulario.Size = new Size(551, 531);
+            pn_formulario.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(19, 3);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Horarios";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(70, 111);
+            label2.Name = "label2";
+            label2.Size = new Size(149, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Hora inicio:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Black", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(70, 208);
+            label3.Name = "label3";
+            label3.Size = new Size(138, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Hora final:";
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackColor = Color.FromArgb(92, 64, 51);
+            this.btn_eliminar.Image = Properties.Resources.boton_eliminar;
+            this.btn_eliminar.Location = new Point(926, 685);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new Size(177, 70);
+            this.btn_eliminar.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.btn_eliminar.TabIndex = 3;
+            this.btn_eliminar.TabStop = false;
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.BackColor = Color.FromArgb(92, 64, 51);
+            this.btn_agregar.Image = Properties.Resources.boton_agregar;
+            this.btn_agregar.Location = new Point(1156, 685);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new Size(177, 70);
+            this.btn_agregar.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.btn_agregar.TabIndex = 4;
+            this.btn_agregar.TabStop = false;
+            // 
+            // btn_modificar
+            // 
+            btn_modificar.BackColor = Color.FromArgb(92, 64, 51);
+            btn_modificar.Image = Properties.Resources.boton_modificar;
+            btn_modificar.Location = new Point(689, 685);
+            btn_modificar.Name = "btn_modificar";
+            btn_modificar.Size = new Size(177, 70);
+            btn_modificar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btn_modificar.TabIndex = 5;
+            btn_modificar.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(30, 400);
+            label4.Name = "label4";
+            label4.Size = new Size(59, 25);
+            label4.TabIndex = 0;
+            label4.Text = "label4";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(689, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(206, 48);
+            label5.TabIndex = 1;
+            label5.Text = "HORARIOS";
+            label5.Click += label5_Click;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = Properties.Resources.alarm_50dp_E3E3E3_FILL0_wght400_GRAD0_opsz48;
+            pictureBox4.Location = new Point(901, 3);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(82, 70);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 7;
+            pictureBox4.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(246, 240, 232);
+            panel1.Controls.Add(txt_buscar);
+            panel1.Controls.Add(dgv_horarios);
+            panel1.Controls.Add(label8);
+            panel1.Location = new Point(689, 131);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(644, 531);
+            panel1.TabIndex = 5;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(19, 3);
+            label8.Name = "label8";
+            label8.Size = new Size(103, 38);
+            label8.TabIndex = 0;
+            label8.Text = "Buscar";
+            // 
+            // dgv_horarios
+            // 
+            dgv_horarios.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgv_horarios.Location = new Point(19, 57);
+            dgv_horarios.Name = "dgv_horarios";
+            dgv_horarios.RowHeadersWidth = 62;
+            dgv_horarios.Size = new Size(609, 460);
+            dgv_horarios.TabIndex = 1;
+            // 
+            // txt_buscar
+            // 
+            txt_buscar.Location = new Point(339, 11);
+            txt_buscar.Name = "txt_buscar";
+            txt_buscar.Size = new Size(289, 31);
+            txt_buscar.TabIndex = 5;
+            // 
+            // cmb_horaInicio
+            // 
+            cmb_horaInicio.FormattingEnabled = true;
+            cmb_horaInicio.Location = new Point(246, 114);
+            cmb_horaInicio.Name = "cmb_horaInicio";
+            cmb_horaInicio.Size = new Size(268, 33);
+            cmb_horaInicio.TabIndex = 3;
+            // 
+            // cmb_horafinal
+            // 
+            cmb_horafinal.FormattingEnabled = true;
+            cmb_horafinal.Location = new Point(246, 207);
+            cmb_horafinal.Name = "cmb_horafinal";
+            cmb_horafinal.Size = new Size(268, 33);
+            cmb_horafinal.TabIndex = 4;
+            // 
+            // frm_horarios
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1630, 779);
+            Controls.Add(panel1);
+            Controls.Add(btn_modificar);
+            Controls.Add(this.btn_agregar);
+            Controls.Add(this.btn_eliminar);
+            Controls.Add(pn_formulario);
+            Controls.Add(pnl_superior);
+            Name = "frm_horarios";
+            Text = "frm_horarios";
+            Load += frm_horarios_Load;
+            pnl_superior.ResumeLayout(false);
+            pnl_superior.PerformLayout();
+            pn_formulario.ResumeLayout(false);
+            pn_formulario.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)this.btn_eliminar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)this.btn_agregar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btn_modificar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgv_horarios).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnl_superior;
+        private Panel pn_formulario;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label5;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox btn_modificar;
+        private PictureBox pictureBox4;
+        private Panel panel1;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private ComboBox cmb_horafinal;
+        private ComboBox cmb_horaInicio;
+        private TextBox txt_buscar;
+        private DataGridView dgv_horarios;
+    }
+}
