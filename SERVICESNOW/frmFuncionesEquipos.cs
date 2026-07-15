c32f4efaf01ad1f123cfed2c402550464a1db933
nathaan-hz <20250987@uthh.edu.mx>
2026-07-14 19:11:28 -0600

    autor.: nathan diseño equipos

---
+++ SERVICESNOW/frmFuncionesEquipos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SERVICESNOW
+{
+    public partial class frmFuncionesEquipos : Form
+    {
+        public frmFuncionesEquipos()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmFuncionesEquipos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
