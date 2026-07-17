743e01415f8301b7fc1d2253ccc65be879cd4cd9
nathaan-hz <20250987@uthh.edu.mx>
2026-07-16 19:15:21 -0600

    autor: nathan diseño formularios de salas

---
+++ SERVICESNOW/frm_salas_AGREGAR.Designer.cs
@@ -0,0 +1,396 @@
+﻿namespace SERVICESNOW
+{
+    partial class frm_salas_AGREGAR
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnlSuperior = new Panel();
+            pictureBox1 = new PictureBox();
+            label4 = new Label();
+            pnlSalasAgregar = new Panel();
+            btnLimpiar = new Button();
+            btnGuardar = new Button();
+            txtUbicacion = new TextBox();
+            label12 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            cmbEquipamiento = new ComboBox();
+            txtCantSillas = new TextBox();
+            txtEquipamiento = new Label();
+            cmbTipoSala = new ComboBox();
+            label9 = new Label();
+            label10 = new Label();
+            txtCantMesas = new TextBox();
+            txtCapacidadSala = new TextBox();
+            label6 = new Label();
+            txtDescripcionSala = new TextBox();
+            label3 = new Label();
+            txtNombreSala = new TextBox();
+            label2 = new Label();
+            txtIDSala = new TextBox();
+            label1 = new Label();
+            label5 = new Label();
+            pnlSuperior.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            pnlSalasAgregar.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnlSuperior
+            // 
+            pnlSuperior.BackColor = Color.FromArgb(92, 64, 51);
+            pnlSuperior.Controls.Add(pictureBox1);
+            pnlSuperior.Controls.Add(label4);
+            pnlSuperior.Dock = DockStyle.Top;
+            pnlSuperior.Location = new Point(0, 0);
+            pnlSuperior.Name = "pnlSuperior";
+            pnlSuperior.Size = new Size(1432, 77);
+            pnlSuperior.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.salas;
+            pictureBox1.Location = new Point(765, 9);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(70, 65);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(649, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 41);
+            label4.TabIndex = 0;
+            label4.Text = "SALAS";
+            // 
+            // pnlSalasAgregar
+            // 
+            pnlSalasAgregar.BackColor = Color.FromArgb(217, 199, 184);
+            pnlSalasAgregar.Controls.Add(btnLimpiar);
+            pnlSalasAgregar.Controls.Add(btnGuardar);
+            pnlSalasAgregar.Controls.Add(txtUbicacion);
+            pnlSalasAgregar.Controls.Add(label12);
+            pnlSalasAgregar.Controls.Add(label8);
+            pnlSalasAgregar.Controls.Add(label7);
+            pnlSalasAgregar.Controls.Add(cmbEquipamiento);
+            pnlSalasAgregar.Controls.Add(txtCantSillas);
+            pnlSalasAgregar.Controls.Add(txtEquipamiento);
+            pnlSalasAgregar.Controls.Add(cmbTipoSala);
+            pnlSalasAgregar.Controls.Add(label9);
+            pnlSalasAgregar.Controls.Add(label10);
+            pnlSalasAgregar.Controls.Add(txtCantMesas);
+            pnlSalasAgregar.Controls.Add(txtCapacidadSala);
+            pnlSalasAgregar.Controls.Add(label6);
+            pnlSalasAgregar.Controls.Add(txtDescripcionSala);
+            pnlSalasAgregar.Controls.Add(label3);
+            pnlSalasAgregar.Controls.Add(txtNombreSala);
+            pnlSalasAgregar.Controls.Add(label2);
+            pnlSalasAgregar.Controls.Add(txtIDSala);
+            pnlSalasAgregar.Controls.Add(label1);
+            pnlSalasAgregar.Location = new Point(43, 132);
+            pnlSalasAgregar.Name = "pnlSalasAgregar";
+            pnlSalasAgregar.Size = new Size(1355, 434);
+            pnlSalasAgregar.TabIndex = 4;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.BackColor = Color.FromArgb(92, 64, 51);
+            btnLimpiar.BackgroundImage = Properties.Resources.btn_limpiar;
+            btnLimpiar.BackgroundImageLayout = ImageLayout.Zoom;
+            btnLimpiar.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLimpiar.ForeColor = Color.White;
+            btnLimpiar.Location = new Point(928, 351);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(192, 68);
+            btnLimpiar.TabIndex = 35;
+            btnLimpiar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.BackColor = Color.FromArgb(92, 64, 51);
+            btnGuardar.BackgroundImage = Properties.Resources.btn_guardar;
+            btnGuardar.BackgroundImageLayout = ImageLayout.Zoom;
+            btnGuardar.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGuardar.ForeColor = Color.WhiteSmoke;
+            btnGuardar.Location = new Point(1146, 351);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(192, 68);
+            btnGuardar.TabIndex = 34;
+            btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // txtUbicacion
+            // 
+            txtUbicacion.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtUbicacion.Location = new Point(339, 265);
+            txtUbicacion.Name = "txtUbicacion";
+            txtUbicacion.ReadOnly = true;
+            txtUbicacion.Size = new Size(271, 34);
+            txtUbicacion.TabIndex = 33;
+            txtUbicacion.TabStop = false;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(16, 12);
+            label12.Name = "label12";
+            label12.Size = new Size(180, 31);
+            label12.TabIndex = 32;
+            label12.Text = "Datos de la sala";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(339, 150);
+            label8.Name = "label8";
+            label8.Size = new Size(164, 28);
+            label8.TabIndex = 30;
+            label8.Text = "Cantidad de sillas";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(339, 234);
+            label7.Name = "label7";
+            label7.Size = new Size(99, 28);
+            label7.TabIndex = 21;
+            label7.Text = "Ubicacion";
+            // 
+            // cmbEquipamiento
+            // 
+            cmbEquipamiento.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmbEquipamiento.FormattingEnabled = true;
+            cmbEquipamiento.Location = new Point(630, 181);
+            cmbEquipamiento.Name = "cmbEquipamiento";
+            cmbEquipamiento.Size = new Size(271, 36);
+            cmbEquipamiento.TabIndex = 29;
+            // 
+            // txtCantSillas
+            // 
+            txtCantSillas.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtCantSillas.Location = new Point(339, 181);
+            txtCantSillas.Name = "txtCantSillas";
+            txtCantSillas.ReadOnly = true;
+            txtCantSillas.Size = new Size(113, 34);
+            txtCantSillas.TabIndex = 24;
+            txtCantSillas.TabStop = false;
+            // 
+            // txtEquipamiento
+            // 
+            txtEquipamiento.AutoSize = true;
+            txtEquipamiento.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtEquipamiento.Location = new Point(630, 150);
+            txtEquipamiento.Name = "txtEquipamiento";
+            txtEquipamiento.Size = new Size(134, 28);
+            txtEquipamiento.TabIndex = 28;
+            txtEquipamiento.Text = "Equipamiento";
+            // 
+            // cmbTipoSala
+            // 
+            cmbTipoSala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cmbTipoSala.FormattingEnabled = true;
+            cmbTipoSala.Location = new Point(630, 97);
+            cmbTipoSala.Name = "cmbTipoSala";
+            cmbTipoSala.Size = new Size(271, 36);
+            cmbTipoSala.TabIndex = 27;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(339, 66);
+            label9.Name = "label9";
+            label9.Size = new Size(176, 28);
+            label9.TabIndex = 25;
+            label9.Text = "Cantidad de mesas";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(630, 66);
+            label10.Name = "label10";
+            label10.Size = new Size(116, 28);
+            label10.TabIndex = 26;
+            label10.Text = "Tipo de sala";
+            // 
+            // txtCantMesas
+            // 
+            txtCantMesas.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtCantMesas.Location = new Point(339, 97);
+            txtCantMesas.Name = "txtCantMesas";
+            txtCantMesas.ReadOnly = true;
+            txtCantMesas.Size = new Size(113, 34);
+            txtCantMesas.TabIndex = 22;
+            txtCantMesas.TabStop = false;
+            // 
+            // txtCapacidadSala
+            // 
+            txtCapacidadSala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtCapacidadSala.Location = new Point(16, 371);
+            txtCapacidadSala.Name = "txtCapacidadSala";
+            txtCapacidadSala.ReadOnly = true;
+            txtCapacidadSala.Size = new Size(113, 34);
+            txtCapacidadSala.TabIndex = 17;
+            txtCapacidadSala.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(16, 340);
+            label6.Name = "label6";
+            label6.Size = new Size(104, 28);
+            label6.TabIndex = 18;
+            label6.Text = "Capacidad";
+            // 
+            // txtDescripcionSala
+            // 
+            txtDescripcionSala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtDescripcionSala.Location = new Point(16, 265);
+            txtDescripcionSala.Multiline = true;
+            txtDescripcionSala.Name = "txtDescripcionSala";
+            txtDescripcionSala.ReadOnly = true;
+            txtDescripcionSala.Size = new Size(271, 59);
+            txtDescripcionSala.TabIndex = 15;
+            txtDescripcionSala.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(16, 234);
+            label3.Name = "label3";
+            label3.Size = new Size(199, 28);
+            label3.TabIndex = 16;
+            label3.Text = "Descripción de la sala";
+            // 
+            // txtNombreSala
+            // 
+            txtNombreSala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtNombreSala.Location = new Point(16, 181);
+            txtNombreSala.Name = "txtNombreSala";
+            txtNombreSala.ReadOnly = true;
+            txtNombreSala.Size = new Size(271, 34);
+            txtNombreSala.TabIndex = 13;
+            txtNombreSala.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(16, 150);
+            label2.Name = "label2";
+            label2.Size = new Size(170, 28);
+            label2.TabIndex = 14;
+            label2.Text = "Nombre de la sala";
+            // 
+            // txtIDSala
+            // 
+            txtIDSala.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtIDSala.Location = new Point(16, 97);
+            txtIDSala.Name = "txtIDSala";
+            txtIDSala.ReadOnly = true;
+            txtIDSala.Size = new Size(113, 34);
+            txtIDSala.TabIndex = 11;
+            txtIDSala.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(16, 66);
+            label1.Name = "label1";
+            label1.Size = new Size(180, 28);
+            label1.TabIndex = 12;
+            label1.Text = "ID Sala Automatico";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(43, 80);
+            label5.Name = "label5";
+            label5.Size = new Size(208, 41);
+            label5.TabIndex = 10;
+            label5.Text = "Registrar sala";
+            // 
+            // frm_salas_AGREGAR
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 251, 245);
+            ClientSize = new Size(1432, 592);
+            Controls.Add(pnlSalasAgregar);
+            Controls.Add(pnlSuperior);
+            Controls.Add(label5);
+            Name = "frm_salas_AGREGAR";
+            Text = "frm_salas_AGREGAR";
+            pnlSuperior.ResumeLayout(false);
+            pnlSuperior.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            pnlSalasAgregar.ResumeLayout(false);
+            pnlSalasAgregar.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pnlSuperior;
+        private PictureBox pictureBox1;
+        private Label label4;
+        private Panel pnlSalasAgregar;
+        private Label label5;
+        private TextBox txtCantSillas;
+        private Label label9;
+        private TextBox txtCantMesas;
+        private Label label7;
+        private TextBox txtCapacidadSala;
+        private Label label6;
+        private TextBox txtDescripcionSala;
+        private Label label3;
+        private TextBox txtNombreSala;
+        private Label label2;
+        private TextBox txtIDSala;
+        private Label label1;
+        private Label label8;
+        private ComboBox cmbEquipamiento;
+        private Label txtEquipamiento;
+        private ComboBox cmbTipoSala;
+        private Label label10;
+        private Label label12;
+        private TextBox txtUbicacion;
+        private Button btnGuardar;
+        private Button btnLimpiar;
+    }
+}
